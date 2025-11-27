d1c898d1d5b61725113b6aec7e0c684a6cc29956
ClosRaid <ad-ad2000@inbox.ru>
2025-11-27 17:43:40 +0500

    lelele

---
+++ бубубу.py
@@ -0,0 +1,75 @@
+#1.
+'''
+user_choice = int(input("Введите номер дня недели: "))
+if user_choice == 1:
+    print("пн")
+elif user_choice == 2:
+    print("вт")
+elif user_choice == 3:
+    print("ср")
+elif user_choice == 4:
+    print("чт")
+elif user_choice == 5:
+    print("пт")
+elif user_choice == 6:
+    print("сб")
+elif user_choice == 7:
+    print("вс")
+else:
+    print()
+'''
+#2.
+'''
+user_choice = int(input("Введите номер месяца: "))
+if user_choice == 1:
+    print("янв")
+elif user_choice == 2:
+    print("фев")
+elif user_choice == 3:
+    print("март")
+elif user_choice == 4:
+    print("апр")
+elif user_choice == 5:
+    print("май")
+elif user_choice == 6:
+    print("июнь")
+elif user_choice == 7:
+    print("июль")
+elif user_choice == 8:
+    print("авг")
+elif user_choice == 9:
+    print("сен")
+elif user_choice == 10:
+    print("окт")
+elif user_choice == 11:
+    print("ноя")
+elif user_choice == 12:
+    print("дек")
+else:
+    print()
+'''
+#3.
+'''
+num = int(input("Введите число: "))
+if num < 0:
+    print("число отрицательное")
+elif num == 0:
+    print("число равно нулю")
+elif num > 0:
+    print("Число положительное")
+else:
+    print()
+'''
+#4.
+'''
+num1 = int(input("Введите число: "))
+num2 = int(input("Введите число: "))
+if num1 == num2:
+    print("Числа равны")
+elif num1 > num2:
+    print(num1)
+elif num1 < num2:
+    print(num2)
+else:
+    print()
+'''
