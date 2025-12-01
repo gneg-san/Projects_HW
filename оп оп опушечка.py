1117987c8854a3c30d5d3f8dfa8f2dfa4d758f7e
ClosRaid <ad-ad2000@inbox.ru>
2025-12-01 19:37:52 +0500

    оп оп оп

---
+++ оп оп опушечка.py
@@ -0,0 +1,29 @@
+#1.
+'''
+def ooo():
+    print("\nдонт компер юр селф....\n",
+    "бил гейтс")
+ooo()
+'''
+#2.
+'''
+def zzz():
+    value1 = int(input())
+    value2 = int(input())
+    for i in range(value1, value2):
+        if i % 2 == 0:
+            print(i)
+zzz()
+'''
+#3.
+'''
+def wow():
+     a = input()
+     b = int(input(" 1 - true, 2 - false "))
+     if b == 1:
+         print() # заполненый квадрат
+     elif b == 2:
+         print() # пустой квадрат
+     else:
+         print()
+'''
